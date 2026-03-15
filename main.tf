8d74d74afffaa09d055ce705d72813c2c4a24451
patricengato <patrice.ngato@google.com>
2026-03-15 12:28:02 -0400

    ajout virtual network terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,24 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-ngato"
-  location = "eastus"
+  name     = "rg-terraform-demo"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
