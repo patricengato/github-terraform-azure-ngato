7021ee3e71d0a413e95f4799cec6bab50ffa44c9
patricengato <patrice.ngato@google.com>
2026-03-15 13:04:29 -0400

    deployment VM terraform

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "terraform-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_B1ms"
   admin_username      = "azureuser"
 
   network_interface_ids = [
