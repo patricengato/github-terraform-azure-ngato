39a44a646bb47f6a602cb8431e270f0266bcf406
patricengato <patrice.ngato@google.com>
2026-03-15 15:06:47 -0400

    deployment VM terraform

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "terraform-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "SStandard_A1_v2"
+  size                = "Standard_B1s"
   admin_username      = "azureuser"
   admin_password      = "Password1234@!"
   disable_password_authentication = false
