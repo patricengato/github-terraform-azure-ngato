7cc8031438e5821fbd680596ce3fbe3d0f50ee8b
patricengato <patrice.ngato@google.com>
2026-03-15 15:10:51 -0400

    deployment VM terraform

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-ngato"
-  location = "East US 2"
+  location = "Canada Central"
 }
 
 # Virtual Network
